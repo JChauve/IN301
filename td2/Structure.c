dd09cbebd585ff6cb60d4bdb82e40915685d55f7
JChauve <joseph.chauve@live.fr>
2015-10-08 12:22:48 +0200

    Modif Stucture.c

--- td2/Structure.c
+++ td2/Structure.c
@@ -9,10 +9,62 @@ struct Tableau{
 };
 typedef struct Tableau Tableau;
 
-//fonction principale
+int alea(int n);
+Tableau initialiser(Tableau T);
+void afficher(Tableau T);
+int produit(Tableau T);
+
+//fonction Main
 int main(){
 
-	printf("Taille memoire de Tableau : %d", sizeof(Tableau));
-		
+	srand(time(NULL));
+	
+	Tableau T;
+	int prod;
+	
+	T = initialiser(T);
+	afficher(T);
+	prod = produit(T);
+	printf("Le produit des elements du tableau vaut : %d.", prod);
+	
   return 0;
+}
+
+// Fonctions
+int alea(int n)
+{
+	int a;
+	a = rand() % n;
+	return a;
+}
+
+Tableau initialiser(Tableau T)
+{
+	T.taille = 10;
+	int i;
+	for(i=0; i<10; i++)
+	{
+		T.tab[i] = alea(20);
+	}
+	return T;
+}
+
+void afficher(Tableau T)
+{
+	int i;
+	for(i=0; i<T.taille; i++)
+	{
+		printf("l'element de la case %d vaut %d.\n",i,T.tab[i]);
+	}
+}
+
+int produit(Tableau T)
+{
+	int resultat=1;
+	int i;
+	for(i=0; i<T.taille; i++)
+	{
+		resultat = resultat*T.tab[i];
+	}
+	return resultat;
 }
